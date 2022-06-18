bac202ca0efa1559b92b375ac21d7c468eb087e3
jose <tonyjimenez099.tj@gmail.com>
2022-06-17 21:11:01 -0500

    commit tree 0a0dc01607b19445d503c806438529fdeb8aebf3

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+    input.addEventListener('click', function() {
+        alert('i was clicked!');
+    });
 }
+addingEventListener();
